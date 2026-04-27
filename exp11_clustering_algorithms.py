fc894822756412a6e9fa71794ed37d7c9e465728
krishramoju2 <futureai.incorporated@gmail.com>
2026-04-27 18:32:00 +0530

    Update exp11_clustering_algorithms.py

--- exp11_clustering_algorithms.py
+++ exp11_clustering_algorithms.py
@@ -3,7 +3,7 @@ print("Experiment 11: Clustering Algorithms")
 print("="*50)
 
 from sklearn.datasets import load_iris
-from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
 from sklearn.metrics import accuracy_score
 
 iris = load_iris()
@@ -11,12 +11,16 @@ X, y = iris.data, iris.target
 
 kmeans = KMeans(n_clusters=3)
 agglo = AgglomerativeClustering(n_clusters=3)
+dbscan = DBSCAN()
 
 kmeans_labels = kmeans.fit_predict(X)
 print("\nKMeans:")
-print("Accuracy:", accuracy_score(y, kmeans_labels), 4)
-
+print("Accuracy:", round(accuracy_score(y, kmeans_labels), 4))
 
 agglo_labels = agglo.fit_predict(X)
 print("\nAgglomerative:")
-print("Accuracy:", accuracy_score(y, agglo_labels))
+print("Accuracy:", round(accuracy_score(y, agglo_labels), 4))
+
+dbscan_labels = dbscan.fit_predict(X)
+print("\nDBSCAN:")
+print("Clusters found:", len(set(dbscan_labels)))
