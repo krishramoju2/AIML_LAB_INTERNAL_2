e2574495c3b414a4a30a11641c10643b1d6e7516
krishramoju2 <futureai.incorporated@gmail.com>
2026-04-27 17:36:06 +0530

    Update exp9_decision_tree_ensembles.py

--- exp9_decision_tree_ensembles.py
+++ exp9_decision_tree_ensembles.py
@@ -12,7 +12,7 @@ y = data['Outcome']
 for c in ['Glucose', 'BloodPressure', 'BMI']:
     X[c] = X[c].replace(0, X[c].mean())
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 print("Model,Accuracy")
 
