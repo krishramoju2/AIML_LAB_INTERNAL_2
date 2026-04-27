9316c4d2f41aa7a8d81c3289cc87db8fa7be72f4
krishramoju2 <futureai.incorporated@gmail.com>
2026-04-27 17:00:18 +0530

    Update exp10_svm_character_recognition.py

--- exp10_svm_character_recognition.py
+++ exp10_svm_character_recognition.py
@@ -8,7 +8,7 @@ digits = load_digits()
 X = digits.images.reshape(len(digits.images), -1)
 y = digits.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
